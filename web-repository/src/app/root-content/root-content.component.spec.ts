2b73615d75dea7603890b0212ebc030c4332de88
Alexander Anseev <anseev704@gmail.com>
2023-05-02 23:29:39 -0400

    Hello section ready
    - Finished hello section
    - Added links to sidenav
    - Added progress bars to sidenav
    
    TODO:
    - add bg color change
    - add progress bar change
    - add other sections

---
+++ web-repository/src/app/root-content/root-content.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RootContentComponent } from './root-content.component';
+
+describe('RootContentComponent', () => {
+  let component: RootContentComponent;
+  let fixture: ComponentFixture<RootContentComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RootContentComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(RootContentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ web-repository/src/app/root-sidenav/root-sidenav.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root-sidenav',
+  templateUrl: './root-sidenav.component.html',
+  styleUrls: ['./root-sidenav.component.css']
+})
+export class RootSidenavComponent {
+
+}
