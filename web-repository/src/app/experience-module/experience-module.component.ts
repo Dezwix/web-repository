c3d41acb25a1bca0a714c56c341627ff39a6f0f0
Alexander Anseev <anseev704@gmail.com>
2023-05-31 23:49:48 -0400

    Added component experience module

---
+++ web-repository/src/app/experience-module/experience-module.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-experience-module',
+  templateUrl: './experience-module.component.html',
+  styleUrls: ['./experience-module.component.scss']
+})
+export class ExperienceModuleComponent {
+
+}
