74a71338424928155d35e5658ab659e65a8710c9
Alexander Anseev <alexander.anseev@gmail.com>
2023-06-01 23:57:40 -0400

    Started experience module template

--- web-repository/src/app/experience-module/experience-module.component.ts
+++ web-repository/src/app/experience-module/experience-module.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./experience-module.component.scss']
 })
 export class ExperienceModuleComponent {
-
+  public logoPath: string = "/assets/img/dezwixVector.png"
+  public title: string = "title";
+  public dateStart: Date = new Date(1970, 12, 1);
+  public dateEnd: Date = new Date(1970, 12, 2);
+  public companyName: string = "company";
+  public location: string = "location";
+  public description: string = "description";
 }
