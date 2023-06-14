4b43fa647b203ed495fc43cc08cedb0385f47e3b
Alexander Anseev <anseev704@gmail.com>
2023-06-13 22:10:10 -0400

    Added observer and fixed sideless experience view
    - Added another intersection observer to observe specifically the experience section, since it's considerably larger than other sections
    - Added centering elements within the experience wrap for a sideless view
    - Increased width at which sidenav disappears to 1150px

--- web-repository/src/app/app.component.ts
+++ web-repository/src/app/app.component.ts
@@ -9,6 +9,7 @@ export class AppComponent {
   title = 'web-repository';
 
   private observer!: IntersectionObserver;
+  private experienceObserver!: IntersectionObserver;
   public helloInView = false;
   public experienceInView = false;
   public projectsInView = false;
@@ -32,6 +33,17 @@ export class AppComponent {
       rootMargin: '0px',
       threshold: 0.45
     });
+
+    this.experienceObserver = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.target.id === 'experience-section') {
+          this.experienceInView = entry.isIntersecting;
+        }
+      });
+    }, {
+      rootMargin: '0px',
+      threshold: 0.02
+    });
   }
 
   ngAfterViewInit() {
@@ -41,7 +53,7 @@ export class AppComponent {
     const contactComponent = document.querySelector('#contact-section')!;
 
     this.observer.observe(helloComponent);
-    this.observer.observe(experienceComponent);
+    this.experienceObserver.observe(experienceComponent);
     this.observer.observe(projectsComponent);
     this.observer.observe(contactComponent);
   }
